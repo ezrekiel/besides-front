5d90b4d4a6903f6cb17079de0b2a2ebac4552711
Alexis Lelardoux <alelardoux@garouda.com>
2024-10-18 17:10:22 +0200

    cleaned this turbid mess

--- modules/dashboard.module.js
+++ modules/dashboard.module.js
@@ -2,7 +2,7 @@ import { authService } from '../services/auth.service.js';
 import { userService } from '../services/user.service.js';
 import { offerService } from '../services/offer.service.js';
 import { domHelper } from '../helpers/dom.helper.js';
-import { EDIT_SVG, DELETE_SVG } from '../helpers/constants.helper.js';
+import { EDIT_SVG, DELETE_SVG, PROPERTIES_NAMES_MAPPING } from '../helpers/constants.helper.js';
 
 const usersManagementButton = document.getElementById('users-management');
 const companiesManagementButton = document.getElementById('companies-management');
@@ -14,30 +14,13 @@ const updateResourceForm = document.getElementById('updateResourceForm');
 const deleteResourceForm = document.getElementById('deleteResourceForm');
 const output = document.getElementById('output');
 
-const propertiesNameMapping = {
-	"userID" : "Id",
-	"companyID" : "Id entreprise",
-	"firstname" : "Prénom",
-	"lastname" : "Nom",
-	"username" : "E-mail",
-
-	"id" : "Id",
-	"companyName": "Entreprise",
-    "legalStatus": "Statut legal",
-    "activitySector": "Secteur d'activité",
-
-	"offerID": "Id",
-    "title": "Offre",
-	"contractType": "Contrat"
-}
-
 function createResourceTable(parentElement, resources) {
 	const table = domHelper.createHTMLElement('table', {}, parentElement);
 
 	//Table Head
 	const tableHead = domHelper.createHTMLElement('thead', {}, table);
 	const tableHeadRow = domHelper.createHTMLElement('tr', {}, tableHead);
-	Object.keys(resources[0]).forEach(key => domHelper.createHTMLElement('th', {}, tableHeadRow, propertiesNameMapping[key]));
+	Object.keys(resources[0]).forEach(key => domHelper.createHTMLElement('th', {}, tableHeadRow, PROPERTIES_NAMES_MAPPING[key]));
 	domHelper.createHTMLElement('th', {}, tableHeadRow, 'Editer');
 	domHelper.createHTMLElement('th', {}, tableHeadRow, 'Supprimer');
 
@@ -49,59 +32,6 @@ function createResourceTable(parentElement, resources) {
 		domHelper.createHTMLElement('td', {}, tableBodyRow, EDIT_SVG);
 		domHelper.createHTMLElement('td', {}, tableBodyRow, DELETE_SVG);
 	});
-}
-
-function createTableRow(object) {
-	const tableBody = document.getElementById('table-body');
-	const tableBodyRow = document.createElement('tr');
-	Object.keys(object).forEach(key => {
-		const objectProperty = document.createElement('td');
-		objectProperty.textContent = object[key];
-		tableBodyRow.appendChild(objectProperty);
-	});
-
-	
-	const tableBodyColumn5 = document.createElement('td');
-	const tableBodyColumn6 = document.createElement('td');
-
-    const svgElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    svgElement.setAttribute("width", "24px");
-    svgElement.setAttribute("height", "24px");
-    svgElement.setAttribute("viewBox", "0 0 24 24");
-    svgElement.setAttribute("fill", "none");
-    svgElement.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-
-    const pathElement = document.createElementNS("http://www.w3.org/2000/svg", "path");
-    pathElement.setAttribute("fill-rule", "evenodd");
-    pathElement.setAttribute("clip-rule", "evenodd");
-    pathElement.setAttribute("d", "M21.1213 2.70705C19.9497 1.53548 18.0503 1.53547 16.8787 2.70705L15.1989 4.38685L7.29289 12.2928C7.16473 12.421 7.07382 12.5816 7.02986 12.7574L6.02986 16.7574C5.94466 17.0982 6.04451 17.4587 6.29289 17.707C6.54127 17.9554 6.90176 18.0553 7.24254 17.9701L11.2425 16.9701C11.4184 16.9261 11.5789 16.8352 11.7071 16.707L19.5556 8.85857L21.2929 7.12126C22.4645 5.94969 22.4645 4.05019 21.2929 2.87862L21.1213 2.70705ZM18.2929 4.12126C18.6834 3.73074 19.3166 3.73074 19.7071 4.12126L19.8787 4.29283C20.2692 4.68336 20.2692 5.31653 19.8787 5.70705L18.8622 6.72357L17.3068 5.10738L18.2929 4.12126ZM15.8923 6.52185L17.4477 8.13804L10.4888 15.097L8.37437 15.6256L8.90296 13.5112L15.8923 6.52185ZM4 7.99994C4 7.44766 4.44772 6.99994 5 6.99994H10C10.5523 6.99994 11 6.55223 11 5.99994C11 5.44766 10.5523 4.99994 10 4.99994H5C3.34315 4.99994 2 6.34309 2 7.99994V18.9999C2 20.6568 3.34315 21.9999 5 21.9999H16C17.6569 21.9999 19 20.6568 19 18.9999V13.9999C19 13.4477 18.5523 12.9999 18 12.9999C17.4477 12.9999 17 13.4477 17 13.9999V18.9999C17 19.5522 16.5523 19.9999 16 19.9999H5C4.44772 19.9999 4 19.5522 4 18.9999V7.99994Z");
-    pathElement.setAttribute("fill", "#000000");
-
-    svgElement.appendChild(pathElement);
-
-	const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-	svg.setAttribute("width", "24px");
-	svg.setAttribute("height", "24px");
-	svg.setAttribute("viewBox", "0 0 24 24");
-	svg.setAttribute("fill", "none");
-	svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-
-
-	const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-	path.setAttribute("d", "M10 11V17M14 11V17M4 7H20M6 7H12 18V18C18 19.6569 16.6569 21 15 21H9C7.3431 21 6 19.6569 6 18V7ZM9 5C9 3.8954 9.8954 3 11 3H13C14.1046 3 15 3.8954 15 5V7H9V5Z");
-	path.setAttribute("stroke", "#000000");
-	path.setAttribute("stroke-width", "2");
-	path.setAttribute("stroke-linecap", "round");
-	path.setAttribute("stroke-linejoin", "round");
-
-	svg.appendChild(path);
-
-	tableBodyColumn5.appendChild(svgElement);
-	tableBodyColumn6.appendChild(svg);
-	tableBodyRow.appendChild(tableBodyColumn5);
-	tableBodyRow.appendChild(tableBodyColumn6);
-
-	tableBody.appendChild(tableBodyRow);
 }
 
 window.addEventListener('DOMContentLoaded', async () => {
@@ -116,21 +46,11 @@ window.addEventListener('DOMContentLoaded', async () => {
 		username.textContent = `${currentUser.firstname} ${currentUser.lastname}`;
 	}
 });
-/*
-unloadTable.addEventListener('click', async () => {
-	try {
-		if(tableElement) {
-			tableElement.remove(); // supprime le tableau si deja existant
-		}
-	} catch (error) {
-		console.log(error);
-	}
-});*/
 
 // Loads the list of all the users
 usersManagementButton.addEventListener('click', async () => {
 	try {
-		//unloadTable();
+		//unloadTable(); //TODO : Proper implementation of this shit
 
 		const pageInformations = document.getElementById('page-name');
 		const contentTitle = document.getElementById('content-name');
@@ -142,43 +62,6 @@ usersManagementButton.addEventListener('click', async () => {
 		if (!users) return;
 		const tableContainer = document.getElementById('content-table');
 		createResourceTable(tableContainer, users);
-/*
-		//1ere étape : construire le tableau
-		const tableElement = document.createElement('table');
-
-		//2ème étape : construire le thead
-
-		const tableHead = document.createElement('thead');
-		const tableHeadRow = document.createElement('tr');
-
-		Object.keys(users[0]).forEach(key => {
-			const objectProperty = document.createElement('th');
-			objectProperty.textContent = propertiesNameMapping[key];
-			tableHeadRow.appendChild(objectProperty);
-		});
-
-		const tableHeadColumn5 = document.createElement('th');
-		tableHeadColumn5.textContent = 'Edition';
-		
-		const tableHeadColumn6 = document.createElement('th');
-		tableHeadColumn6.textContent = 'Supprimer';
-
-		tableElement.appendChild(tableHead);
-		tableHead.appendChild(tableHeadRow);
-		tableHeadRow.appendChild(tableHeadColumn5);
-		tableHeadRow.appendChild(tableHeadColumn6);
-
-		//3ème étape : construire le tbody
-		const tableBody = document.createElement('tbody');
-		tableBody.setAttribute('id', 'table-body');
-		tableElement.appendChild(tableBody);
-		tableContainer.appendChild(tableElement);
-		
-		//4ème étape : insérer les tr
-		users.forEach(user => {
-			createTableRow(user);
-		});*/
-
 	} catch(error) {
 		console.log(error);
 	}
@@ -194,45 +77,10 @@ companiesManagementButton.addEventListener('click', async () => {
 		contentTitle.textContent = 'Entreprises';
 
 		const companies = await companyService.getAllCompanies();
-
 		if (!companies) return;
 
 		const tableContainer = document.getElementById('content-table');
-
-		//1ere étape : construire le tableau
-		const tableElement = document.createElement('table');
-
-		//2ème étape : construire le thead
-		const tableHead = document.createElement('thead');
-		const tableHeadRow = document.createElement('tr');
-
-		Object.keys(users[0]).forEach(key => {
-			const objectProperty = document.createElement('th');
-			objectProperty.textContent = propertiesNameMapping[key];
-			tableHeadRow.appendChild(objectProperty);
-		});
-
-		const tableHeadColumn5 = document.createElement('th');
-		tableHeadColumn5.textContent = 'Edition';
-		
-		const tableHeadColumn6 = document.createElement('th');
-		tableHeadColumn6.textContent = 'Supprimer';
-
-		tableElement.appendChild(tableHead);
-		tableHead.appendChild(tableHeadRow);
-		tableHeadRow.appendChild(tableHeadColumn5);
-		tableHeadRow.appendChild(tableHeadColumn6);
-
-		//3ème étape : construire le tbody
-		const tableBody = document.createElement('tbody');
-		tableBody.setAttribute('id', 'table-body');
-		tableElement.appendChild(tableBody);
-		tableContainer.appendChild(tableElement);
-		
-		//4ème étape : insérer les tr
-		companies.forEach(company => {
-			createTableRow(company);
-		});
+		createResourceTable(tableContainer, companies);
 	} catch(error) {
 		console.log(error);
 	}
@@ -241,54 +89,16 @@ companiesManagementButton.addEventListener('click', async () => {
 // Loads the list of all the offers
 offersManagementButton.addEventListener('click', async () => {
 	try {
-		
 		const pageInformations = document.getElementById('page-name');
 		const contentTitle = document.getElementById('content-name');
 		pageInformations.textContent = 'Gestion offres';
 		contentTitle.textContent = 'Offres';
 
 		const offers = await offerService.getAllOffers();
-
 		if (!offers) return;
 
 		const tableContainer = document.getElementById('content-table');
-
-		//1ere étape : construire le tableau
-		const tableElement = document.createElement('table');
-
-		//2ème étape : construire le thead
-		const tableHead = document.createElement('thead');
-		const tableHeadRow = document.createElement('tr');
-
-		const tableData = offers[0].getTableData();
-		console.log(tableData);
-		Object.keys(tableData).forEach(key => {
-			const objectProperty = document.createElement('th');
-			objectProperty.textContent = key;
-			tableHeadRow.appendChild(objectProperty);
-		});
-
-		const tableHeadColumn5 = document.createElement('th');
-		tableHeadColumn5.textContent = 'Edition';
-		
-		const tableHeadColumn6 = document.createElement('th');
-		tableHeadColumn6.textContent = 'Supprimer';
-
-		tableElement.appendChild(tableHead);
-		tableHead.appendChild(tableHeadRow);
-		tableHeadRow.appendChild(tableHeadColumn5);
-		tableHeadRow.appendChild(tableHeadColumn6);
-
-		//3ème étape : construire le tbody
-		const tableBody = document.createElement('tbody');
-		tableBody.setAttribute('id', 'table-body');
-		tableElement.appendChild(tableBody);
-		tableContainer.appendChild(tableElement);
-		
-		//4ème étape : insérer les tr
-		offers.forEach(offer => {
-			createTableRow(offer.getTableData());
-		});
+		createResourceTable(tableContainer, companies);
 	} catch(error) {
 		console.log(error);
 	}
